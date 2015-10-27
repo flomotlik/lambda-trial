d6e3947b4e218cb4d69ce6d4556dc3b3966756f2
Florian Motlik <flomotlik@gmail.com>
2015-10-26 22:29:31 -0400

    Use Lambda Version to point to new deployed Version

--- LambdaTest.js
+++ LambdaTest.js
@@ -5,5 +5,5 @@ exports.handler = function(event, context) {
     console.log('Who = ' + event.key1);
     console.log('is = ' + event.key2);
     console.log('awesome = ' + event.key3);
-    context.done(null, 'Hello World');  // SUCCESS with message
+    context.done(null, 'Hello World - DATE');  // SUCCESS with message
 };
